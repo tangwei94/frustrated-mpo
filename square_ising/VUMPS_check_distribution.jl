4878c653b1025a0f4a083d9d67a0e6089ffdb7e9
Wei Tang <wei.tang.nju@gmail.com>
2023-05-26 16:08:46 +0200

    put circular_mps in utils.jl

--- square_ising/VUMPS_check_distribution.jl
+++ square_ising/VUMPS_check_distribution.jl
@@ -10,45 +10,17 @@ include("../utils.jl");
 
 @load "square_ising/data/VUMPS_hermitian_betac.jld2" ψs fs
 
-function circular_mps(ψ, L, G, ϵ=1e-8)
+function circular_mps(ψ, L, τ, ϵ=1e-8)
+    G = exp(-τ * σx)
     @tensor A[-1 -2 ; -3] := ψ.AL[1][-1 1; -3] * G[-2 ; 1]
-    sp = left_virtualspace(ψ, 1)
-    B = permute(isomorphism(ℂ^1*sp', sp), (1,), (2, 3,))
-    t = id(sp)
-    M = B
-    # As = typeof(A)[]
-    # for ix in 1:L
-    #     push!(As, L1)
-    # end
-    As = map(1:L) do ix
-        if ix < L
-            @tensor A1[-1 -2 ; -3 -4] := M[-1 ; 1 2] * A[1 -2 ; -3] * t[2; -4]
-            L1, M = leftorth(A1, (1, 2), (3, 4))
-        else
-            @tensor A1[-1 -2 ; -3] := M[-1; 1 2] * A[1 -2; 3] * t[2; 4] * B'[3 4 ; -3]
-            L1, M = leftorth(A1, (1, 2), (3,))
-        end
-        return L1
-    end
-
-    M = id(ℂ^1)
-    ix = 1
-    for ix in L:-1:1
-        A1 = As[ix] * M
-        M, S, R1 = tsvd(A1, (1,), (2, 3), trunc=truncerr(ϵ))
-        As[ix] = permute(R1, (1, 2), (3,))
-        M = M * S
-    end
-
-    return FiniteMPS(As)
+    return circular_mps(A, L, ϵ)
 end
 
 L = 16
 ψ = ψs[4]
 τ = 0.
-G = exp(-τ * σx)
 
-ψlp = circular_mps(ψ, L, G);
+ψlp = circular_mps(ψ, L, τ);
 
 Uy = DenseMPO(fill(add_util_leg(exp(-im*pi*σy/4)), L))
 function meas_σx()
--- utils.jl
+++ utils.jl
@@ -255,6 +255,40 @@ function convert_to_mat(𝕋::DenseMPO)
     return 𝕋mat
 end
 
+
+"""
+    circular_mps(A, L, ϵ=1e-8)
+
+    Construct a uniform circular MPS using the given MPS tensor `A`. Then convert this this circular MPS to an open-boundary MPS.
+"""
+function circular_mps(A::MPSKit.GenericMPSTensor, L::Int, ϵ::Real=1e-8)
+    sp = MPSKit._firstspace(A)
+    B = permute(isomorphism(ℂ^1*sp', sp), (1,), (2, 3,))
+    t = id(sp)
+    M = B
+    As = map(1:L) do ix
+        if ix < L
+            @tensor A1[-1 -2 ; -3 -4] := M[-1 ; 1 2] * A[1 -2 ; -3] * t[2; -4]
+            L1, M = leftorth(A1, (1, 2), (3, 4))
+        else
+            @tensor A1[-1 -2 ; -3] := M[-1; 1 2] * A[1 -2; 3] * t[2; 4] * B'[3 4 ; -3]
+            L1, M = leftorth(A1, (1, 2), (3,))
+        end
+        return L1
+    end
+
+    M = id(ℂ^1)
+    ix = 1
+    for ix in L:-1:1
+        A1 = As[ix] * M
+        M, S, R1 = tsvd(A1, (1,), (2, 3), trunc=truncerr(ϵ))
+        As[ix] = permute(R1, (1, 2), (3,))
+        M = M * S
+    end
+
+    return FiniteMPS(As)
+end
+
 """
     perfect_sampling(ψ::FiniteMPS)
 
